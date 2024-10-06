5ccf5aaaa3e44707e1957f7c3784f4b2ecb2db27
abdulh70 <abdulazizh70@gmail.com>
2024-10-06 23:13:13 +0300

    addded file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const myarray=[]
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  mysubmitt();
+  
 });
+
+function mysubmitt(event){
+  event.preventDefault()
+  const formDataa=new FormData(event.target)
+  const x=Object.fromEntries(formDataa.entries())
+  console.log(x)
+
+}
+document.getElementById("create-task-form").addEventListener('submit',mysubmitt)
+//console.log(myarray)
